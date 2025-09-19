dfa61b05bca3c8be1929c7c2e3613e8f45946553
Felipe <alunopython1@gmail.com>
2025-09-19 15:14:31 -0300

    Add files via upload

---
+++ Modulo 1/Loopings/1.1tabuada.py
@@ -0,0 +1,7 @@
+numero = int(input('Digite um número de 1 a 10: '))
+
+# Síntaxe do for "for i in range(ínicio, final):
+# Comando a ser executado"
+
+for i in range(1, 11):
+    print(f"{i} x {numero} = {i * numero}")
